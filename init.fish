93f9e30df0b24af9eb832c09830cff4741f00993
Kyle Whittington <kthomaswhittington@gmail.com>
2015-10-15 15:04:15 -0500

    Move cocos-js folder to /usr/local/Cellar/

--- init.fish
+++ init.fish
@@ -3,6 +3,6 @@
 set -x JAVA_HOME /Library/Java/Home
 
 # Cocos JS
-set -x  COCOS_CONSOLE_ROOT /Users/kwhittin/Downloads/cocos2d-js/tools/cocos2d-console/bin
+set -x COCOS_CONSOLE_ROOT /usr/local/Cellar/cocos2d-js/tools/cocos2d-console/bin
 set -gx PATH $COCOS_CONSOLE_ROOT $PATH
 set -x ANT_ROOT /usr/local/bin/
