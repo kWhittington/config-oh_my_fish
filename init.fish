92b25ed7b55251b0150da9468b6e3ead74386911
Kyle Whittington <kthomaswhittington@gmail.com>
2015-11-10 09:55:48 -0600

    Add `start_handler_dev`
    This function sets environment variables needed for Handler's Docker
    images and then starts the Docker daemon.

--- init.fish
+++ init.fish
@@ -7,6 +7,17 @@ set -x COCOS_CONSOLE_ROOT /usr/local/Cellar/cocos2d-js/tools/cocos2d-console/bin
 set -gx PATH $COCOS_CONSOLE_ROOT $PATH
 set -x ANT_ROOT /usr/local/bin/
 
+function start_handler_dev
+  # Handler environment variables for db connection:
+  set -x ALERTBUSDB_HOST (docker-machine ip default)
+  set -x ALERTBUSDB_NAME ongo_dev
+  set -x ALERTBUSDB_PASSWORD newcontext
+  set -x CONSOLEDB_HOST (docker-machine ip default)
+  set -x CONSOLEDB_NAME fxs_console_development
+  set -x CONSOLEDB_PASSWORD newcontext
+  docker_daemon_start
+end
+
 function docker_daemon_start
   bash --login '/Applications/Docker/Docker Quickstart Terminal.app/Contents/Resources/Scripts/start.sh'
 end
